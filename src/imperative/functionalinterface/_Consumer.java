8c4f799ad16bf33bd5250c15673101c7ab32cf4c
AkshayTakke <akshaytakke4u@gmail.com>
2025-10-27 23:47:10 +0000

    functional programming initial commit #1

---
+++ src/imperative/functionalinterface/_Consumer.java
@@ -0,0 +1,54 @@
+package imperative.functionalinterface;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class _Consumer {
+    public static void main(String[] args) {
+        // Normal java Function
+        Customer maria = new Customer("Maria", "9999");
+        greetCustomer(maria);
+
+        // Normal multiple input java Function
+        greetCustomer(maria, true);
+
+        // Consumer Functional interface
+        greetCustomerConsumer.accept(new Customer("Maria", "9999"));
+
+        // Bi Consumer implementation Function
+        greetCustomerBiConsumer.accept(maria, false);
+    }
+
+    // Bi Consumer implementation
+    static BiConsumer<Customer, Boolean> greetCustomerBiConsumer = ((customer, showPhoneNumber) ->
+            System.out.println("Hello " + customer.customerName + ", " +
+                    "thanks for registering phone number " + (showPhoneNumber ? customer.customerPhoneNumber : "*****"))
+            );
+
+    // Consumer Functional interface
+    static Consumer<Customer> greetCustomerConsumer = customer ->
+            System.out.println("Hello " + customer.customerName + ", " +
+                    "thanks for registering phone number " + customer.customerPhoneNumber);
+
+    // Normal java Function
+    static void greetCustomer(Customer customer) {
+        System.out.println("Hello " + customer.customerName + ", " +
+                "thanks for registering phone number " + customer.customerPhoneNumber);
+    }
+
+    // Normal java Function of multiple input
+    static void greetCustomer(Customer customer, boolean showPhoneNumber) {
+        System.out.println("Hello " + customer.customerName + ", " +
+                "thanks for registering phone number " + (showPhoneNumber ? customer.customerPhoneNumber : "*****"));
+    }
+
+    static class Customer {
+        private final String customerName;
+        private final String customerPhoneNumber;
+
+        Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+}
---
+++ src/imperative/functionalinterface/_Function.java
@@ -0,0 +1,44 @@
+package imperative.functionalinterface;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class _Function {
+    public static void main(String[] args) {
+        // Function takes one argument and produces one result
+        int increment = increment(0);
+        System.out.println(increment);
+
+        int increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+
+        int multiply = multiplyBy10.apply(increment2);
+        System.out.println(multiply);
+
+        Function<Integer, Integer> addBy1AndMultiplyBy10 =
+                incrementByOneFunction.andThen(multiplyBy10);
+
+        System.out.println(addBy1AndMultiplyBy10.apply(4));
+
+        // BiFunction takes two argument and produces one result
+        System.out.println(increamentByOneAndMultiplyBiFunction.apply(4, 100));
+    }
+
+    static Function<Integer, Integer> incrementByOneFunction =
+            number -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10 =
+            number -> number * 10;
+
+    static int increment(int number) {
+        return number + 1;
+    }
+
+    static int increamentByOneAndMultiply(int number, int multiplyBy) {
+        return  (number + 1) * multiplyBy;
+    }
+
+    static BiFunction<Integer, Integer, Integer> increamentByOneAndMultiplyBiFunction =
+            (numberToIncrementByOne, numberToMultiply)
+            -> (numberToIncrementByOne + 1) * numberToMultiply;
+}
